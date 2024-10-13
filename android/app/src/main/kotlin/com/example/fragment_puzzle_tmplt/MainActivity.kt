67ee8c247817790768bd3365a015114b8fcff7b9
artem <art.zavtur@gmail.com>
2024-10-13 17:55:27 +0300

    update

---
+++ android/app/src/main/kotlin/com/example/fragment_puzzle_tmplt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fragment_puzzle_tmplt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/neludim/bitcoin_chat/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.neludim.bitcoin_chat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
